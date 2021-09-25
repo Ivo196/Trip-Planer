546f02d86a3470c79661a6e1011f0c6b551629ff
Ivo196 <ivo.tonioni@gmail.com>
2021-09-25 17:29:35 -0300

    Create Trip Planner.js

---
+++ Trip Planer JS/Trip Planner.js
@@ -0,0 +1,8 @@
+function main() {
+    var distance = parseInt(readLine(), 10);
+    //tu código va aquí
+    var distance1= distance/40;
+    var time= distance1*60;
+    console.log(time)
+    
+} 
